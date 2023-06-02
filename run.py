89dad3ffeb9a35d1bb1dc55d5b712674f9b3469b
David J-F <davidjf97@yahoo.ie>
2023-06-02 19:21:57 +0000

    Edit layout of form to match python app and google sheet

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+"""
+This module retrieves and displays guest information from a Google Sheets
+spreadsheet.
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Set up credentials to access Google Sheets API
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -10,30 +13,29 @@ SCOPE = [
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-CLIENT = gspread.authorize(SCOPED_CREDS)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('BFGbookings')
 
-# Open the spreadsheet by its title
-spreadsheet = CLIENT.open('BFGbookings')
+responses = SHEET.worksheet('responses')
 
-# Select the worksheet you want to work with (e.g., 'Sheet1')
-worksheet = spreadsheet.worksheet('responses')
-
-# Get all values from the worksheet
-data = worksheet.get_all_values()
+data = responses.get_all_values()
 
 # Iterate over the rows and display guest information room by room
 for row in data[1:]:  # Skip the header row
-    guest_name = row[0]
-    email = row[1]
-    check_in_date = row[2]
-    check_out_date = row[3]
-    room_type = row[4]
-    additional_notes = row[5]
+    response_date = row[0]
+    guest_name = row[1]
+    email = row[2]
+    check_in_date = row[3]
+    check_out_date = row[4]
+    room_type = row[5]
+    additional_notes = row[6]
 
     # Perform any desired processing or display the guest information
+    print(f"Submit Date: {response_date}")
     print(f"Guest Name: {guest_name}")
-    # print(f"Email: {email}")
-    # print(f"Check-in Date: {check_in_date}")
-    # print(f"Check-out Date: {check_out_date}")
-    # print(f"Room Type: {room_type}")
-    # print(f"Additional Notes: {additional_notes}")
+    print(f"Email: {email}")
+    print(f"Check-in Date: {check_in_date}")
+    print(f"Check-out Date: {check_out_date}")
+    print(f"Room Type: {room_type}")
+    print(f"Additional Notes: {additional_notes}")
+    print("---")
