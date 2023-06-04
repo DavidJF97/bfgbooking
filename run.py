9a0fef56d9d0f5d0deac6da91dcab23dbdf16dc2
David J-F <davidjf97@yahoo.ie>
2023-06-04 16:58:45 +0000

    Add prompt to search for guest in spreadsheet

--- run.py
+++ run.py
@@ -20,22 +20,33 @@ responses = SHEET.worksheet('responses')
 
 data = responses.get_all_values()
 
-# Iterate over the rows and display guest information room by room
-for row in data[1:]:  # Skip the header row
-    response_date = row[0]
-    guest_name = row[1]
-    email = row[2]
-    check_in_date = row[3]
-    check_out_date = row[4]
-    room_type = row[5]
-    additional_notes = row[6]
+# Prompt the user to enter the guest's name for search
+guest_name_to_search = input("Enter the name of guest booking was made with: ")
 
-    # Perform any desired processing or display the guest information
-    print(f"Submit Date: {response_date}")
-    print(f"Guest Name: {guest_name}")
-    print(f"Email: {email}")
-    print(f"Check-in Date: {check_in_date}")
-    print(f"Check-out Date: {check_out_date}")
-    print(f"Room Type: {room_type}")
-    print(f"Additional Notes: {additional_notes}")
-    print("---")
+guest_found = False
+
+for row in data[1:]: 
+    guest_name = row[1]
+
+    if guest_name == guest_name_to_search:
+        response_date = row[0]
+        email = row[2]
+        check_in_date = row[3]
+        check_out_date = row[4]
+        room_type = row[5]
+        additional_notes = row[6]
+
+        print(f"Guest Name: {guest_name}")
+        print(f"Submit Date: {response_date}")
+        print(f"Email: {email}")
+        print(f"Check-in Date: {check_in_date}")
+        print(f"Check-out Date: {check_out_date}")
+        print(f"Room Type: {room_type}")
+        print(f"Additional Notes: {additional_notes}")
+        print("---")
+
+        guest_found = True
+        break
+
+if not guest_found:
+    print("Guest not found.")
