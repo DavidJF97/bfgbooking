03aa0186010c68eca618c2793482b2cdf63f2815
David J-F <davidjf97@yahoo.ie>
2023-06-04 18:11:27 +0000

    Add for loop to allow user to try again if guest not found

--- run.py
+++ run.py
@@ -22,32 +22,36 @@ data = responses.get_all_values()
 
 # Prompt the user to enter the guest's name for search
 print("Welcome to THE BUTTERFLY GUESTHOUSE guestbook\n")
-guest_name_to_search = input("Enter the name of guest booking was made with:\n")
-
-guest_found = False
-
-for row in data[1:]: 
-    guest_name = row[1]
-
-    if guest_name == guest_name_to_search:
-        response_date = row[0]
-        email = row[2]
-        check_in_date = row[3]
-        check_out_date = row[4]
-        room_type = row[5]
-        additional_notes = row[6]
-
-        print(f"Guest Name: {guest_name}")
-        print(f"Submit Date: {response_date}")
-        print(f"Email: {email}")
-        print(f"Check-in Date: {check_in_date}")
-        print(f"Check-out Date: {check_out_date}")
-        print(f"Room Type: {room_type}")
-        print(f"Additional Notes: {additional_notes}")
-        print("---")
-
-        guest_found = True
+while True:
+    guest_name_to_search = input("""Enter the name of the guest the booking was made with:\n""")
+    if guest_name_to_search.lower() == "exit":
+        print("Exiting the program.")
         break
 
-if not guest_found:
-    print("Guest not found.")
+    guest_found = False
+
+    for row in data[1:]:
+        guest_name = row[1]
+
+        if guest_name == guest_name_to_search:
+            response_date = row[0]
+            email = row[2]
+            check_in_date = row[3]
+            check_out_date = row[4]
+            room_type = row[5]
+            additional_notes = row[6]
+
+            print(f"Guest Name: {guest_name}")
+            print(f"Submit Date: {response_date}")
+            print(f"Email: {email}")
+            print(f"Check-in Date: {check_in_date}")
+            print(f"Check-out Date: {check_out_date}")
+            print(f"Room Type: {room_type}")
+            print(f"Additional Notes: {additional_notes}")
+            print("---")
+
+            guest_found = True
+            break
+
+    if not guest_found:
+        print("Guest not found. Please try again.")
